0d4586e833b2b34000a3a9b19073ee25b2f89755
Ayaz <Ayaz@DESKTOP-PN90N5M>
2022-09-08 00:35:58 +0530

    Add project files.

---
+++ Students.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Students.aspx.cs" Inherits="aspdatabind.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<%--<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 500px;
+        }
+    </style>
+</head>--%>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table cellpadding="2" cellspacing="5" class="auto-style1">
+                <tr>
+                    <td colspan="2" style="text-align:center;">Employee Record Managment</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Employee Name</td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server" Width="433px"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td class="auto-style2">Employee Gender</td>
+                    <td>
+                        <asp:RadioButton ID="radiobutton1" runat="server" Text="Male" GroupName="Gender" Width="116px"></asp:Radiobutton>
+                        <asp:Radiobutton ID="radiobutton2" runat="server" Text="Female" GroupName="Gender" Width="116px"></asp:Radiobutton>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="auto-style2">Employee Age</td>
+                    <td>
+                        <asp:TextBox ID="TextBox2" runat="server" Width="433px"></asp:TextBox>
+                    </td>
+                </tr>
+               <%-- <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>
+                        &nbsp;</td>
+                </tr>--%>
+                <%--<tr>
+                    <td class="auto-style2">Country</td>
+                    <td>
+                        <asp:DropDownList ID="DropDownList2" runat="server" Width="442px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged" >
+                        </asp:DropDownList>
+                    </td>
+                </tr>--%>
+                <tr>
+                    <td class="auto-style2">State</td>
+                    <td>
+                        <asp:DropDownList ID="DropDownList1" runat="server" Width="442px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">City</td>
+                    <td>
+                        <asp:DropDownList ID="DropDownList2" runat="server" Width="442px" AutoPostBack="True">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="Button1" runat="server" BackColor="#33CC33" BorderColor="#FF66FF" BorderStyle="Groove" Height="44px" OnClick="Button1_Click" Text="Create" Width="176px" />
+                        &nbsp;&nbsp;&nbsp;
+                        <asp:Button ID="Button2" runat="server" BackColor="Aqua" BorderColor="#FF66FF" BorderStyle="Ridge" Height="44px" OnClick="Button2_Click" Text="Update" Width="167px" />
+                        &nbsp;&nbsp;&nbsp;
+                        <asp:Button ID="Button3" runat="server" BackColor="Red" BorderStyle="Groove" Height="46px" OnClick="Button3_Click" Text="Delete" Width="167px" />
+                        &nbsp;&nbsp;&nbsp;
+                        <asp:Button ID="Button4" runat="server" BackColor="#FFFF99" BorderColor="#FF0066" BorderStyle="Groove" Height="44px" Text="Search" Width="167px" OnClick="Button4_Click" />
+                        &nbsp;
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="text-align:center">
+                        <asp:GridView ID="GridView1" runat="server" Width="1095px" BackColor="#99FF66" BorderColor="#FF66FF" BorderStyle="Solid" BorderWidth="2px" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+                        </asp:GridView>
+                    </td>
+                </tr>
+                </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Students.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aspdatabind
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                StateData();
+                GetallData();
+            }
+            
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-PN90N5M;Initial Catalog=AhmadEF;Integrated Security=True");
+
+        public void StateData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from StateName", con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataTextField = "Statename";
+            DropDownList1.DataValueField = "Stateid";
+            DropDownList1.DataBind();
+            con.Close();
+            ListItem Item = new ListItem("Select State", "Select State");
+            Item.Selected = true;
+            DropDownList1.Items.Insert(0, Item);
+        }
+
+        //public void CityData()
+        //{
+        //    con.Open();
+        //    SqlCommand cmd = new SqlCommand("select cid,CName from CityName where StateId = '"+DropDownList1.SelectedValue+"'",con);
+        //    SqlDataAdapter ad = new SqlDataAdapter(cmd);
+        //    DataTable dt = new DataTable();
+        //    ad.Fill(dt);
+        //    DropDownList2.DataSource = dt;
+        //    DropDownList2.DataTextField = "Cityname";
+        //    DropDownList2.DataValueField = "Cityid";
+        //    DropDownList2.DataBind();
+        //    con.Close();
+        //    ListItem Item = new ListItem("Select State", "Select State");
+        //    Item.Selected = true;
+        //    DropDownList3.Items.Insert(0, Item);
+        //}
+
+        public void CityData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select cid, CName from CityName where StateId = '" + DropDownList1.SelectedValue + "'",con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            DropDownList2.DataSource = dt;
+            DropDownList2.DataTextField = "Cityname";
+            DropDownList2.DataValueField = "Cityid";
+            DropDownList2.DataBind();
+            con.Close();
+            ListItem Item = new ListItem("Select City", "Select City");
+            Item.Selected = true;
+            DropDownList2.Items.Insert(0, Item);
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        { 
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Users values('" + TextBox1.Text + "', '" + radiobutton1.Text +radiobutton2.Text+ "','" + int.Parse(TextBox2.Text) + "', '" + DropDownList2.Text + "', '" + DropDownList3.Text + "', '" + DropDownList4.Text + "')",con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data Inserted sucessfully')", true);
+            ClearData();
+            GetallData();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update Studentdata set StudentName = '" + TextBox2.Text + "', StudentAge = '" + int.Parse(TextBox3.Text) + "', Country = '" + DropDownList2.SelectedValue + "', State = '" + DropDownList3.SelectedValue + "', City = '" + DropDownList4.SelectedValue + "' where StudentId = '"+int.Parse(TextBox1.Text)+"'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data Updated sucessfully')", true);
+            ClearData();
+            GetallData();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from Studentdata where StudentId = '" + int.Parse(TextBox1.Text) + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data Deleted sucessfully')", true);
+            ClearData();
+            GetallData();
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            int ID = int.Parse(TextBox1.Text);
+            SqlCommand cmd = new SqlCommand("select * from StudentData where StudentId = '" + ID + "'", con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        public void GetallData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Studentdata",con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        public void ClearData()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+           
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CityData();
+        }
+
+       
+    }
+}
